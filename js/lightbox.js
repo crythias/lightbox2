66361fd83b0d78dd7e5ef3a595af64bd44ba63f8
Gerald Young <gerald.young@noynim.com>
2013-02-27 17:51:49 -0500

    changes made specifically for OTRS. Don't use this for normal lightbox usage.

--- js/lightbox.js
+++ js/lightbox.js
@@ -48,8 +48,8 @@ lightbox = new Lightbox options
   LightboxOptions = (function() {
 
     function LightboxOptions() {
-      this.fileLoadingImage = 'images/loading.gif';
-      this.fileCloseImage = 'images/close.png';
+      this.fileLoadingImage = '/otrs-web/js/thirdparty/lightbox/images/loading.gif';
+      this.fileCloseImage = '/otrs-web/js/thirdparty/lightbox/images/close.png';
       this.resizeDuration = 700;
       this.fadeDuration = 500;
       this.labelImage = "Image";
